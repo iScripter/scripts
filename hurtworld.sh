5fa6209843b4d719dab9e81bf8e7ed3d4a4e1cbc
Marcin Krzempek <polski.hipis.jokerq@gmail.com>
2019-12-17 11:53:57 +0100

    hurtworld script v0.2

--- hurtworld.sh
+++ hurtworld.sh
@@ -4,6 +4,7 @@ apt install lib32gcc1 lib32stdc++6 ca-certificates curl screen -y
 mkdir /opt/steam
 cd /opt/steam
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+rm steamcmd_linux.tar.gz
 ./steamcmd.sh +login anonymous +force_install_dir /opt/steam/hurtworld +app_update 405100 validate +quit
 cd /opt/steam/hurtworld
 chmod +x host.sh
